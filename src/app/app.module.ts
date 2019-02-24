889ef348bb1a60e499cfda8200a090dadb91e678
Olexandr Reshetnyk <olexandr.reshetnyk@gmail.com>
2019-02-24 20:00:24 +0200

    skelet dlya formy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {RentCreationModule} from './rent-creation/rent-creation.module';
 import {AppRoutingModule} from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import {HomeModule} from './home/home.module';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +13,9 @@ import {HomeModule} from './home/home.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HomeModule
+    HomeModule,
+    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
